82a0b80637b3e559621c4f34ea9db9a6615fa8a5
carlos cezar nunes de souza <carlosjsciaa@gmail.com>
2026-01-06 22:04:07 -0300

    fix: remove duplicate route registration causing deploy crash

--- config/routes.php
+++ config/routes.php
@@ -16,5 +16,3 @@ Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@i
 Router::get('/favicon.ico', function () {
     return '';
 });
-
-Router::post('/ai/analyze-pdf', 'App\Controller\AIController@analyzePdf');

3418dc501946dab2a840f8ee76ca846c47c8e46e
carlos cezar nunes de souza <carlosjsciaa@gmail.com>
2026-01-07 20:04:21 -0300

    fix: robust CORS middleware to solve Failed to fetch

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -14,17 +14,21 @@ class CorsMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $response = Context::get(ResponseInterface::class);
-        $response = $response->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD')
-            ->withHeader('Access-Control-Allow-Headers', '*');
-
-        Context::set(ResponseInterface::class, $response);
-
         if ($request->getMethod() === 'OPTIONS') {
-            return $response;
+            $response = Context::get(ResponseInterface::class);
+            return $this->addCorsHeaders($response);
         }
 
-        return $handler->handle($request);
+        $response = $handler->handle($request);
+        return $this->addCorsHeaders($response);
+    }
+
+    protected function addCorsHeaders(ResponseInterface $response): ResponseInterface
+    {
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-API-KEY, DNT, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control')
+            ->withHeader('Access-Control-Max-Age', '1728000');
     }
 }
